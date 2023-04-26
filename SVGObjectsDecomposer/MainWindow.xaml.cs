525db9e9f8a249d93ffcb97e915cfea2fe22fdc6
Tatsuya Yamada <tatsuya19871219@gmail.com>
2023-04-26 12:52:37 +0900

    Remove unused usings

--- SVGObjectsDecomposer/MainWindow.xaml.cs
+++ SVGObjectsDecomposer/MainWindow.xaml.cs
@@ -3,29 +3,12 @@
 
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
 using Svg;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using WinRT.Interop;
-using Windows.ApplicationModel.DataTransfer;
-using System.Threading.Tasks;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using SVGObjectsDecomposer.Models;
-using SVGObjectsDecomposer.OutputWriters;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
